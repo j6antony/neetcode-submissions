1ae42bda1327eea83a7650d8e815c1f7968d391d
Johan Antony <143298867+j6antony@users.noreply.github.com>
2026-08-31 13:58:27 -0400

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.search(board, word, i, j):
+                    return True
+        return False;
+    def search(self, board, word, i, j):
+        print(i, j);
+        if i > len(board) - 1 or i < 0 or j > len(board[0]) - 1 or j < 0: #ensures that i and j are with in range 
+            return False;
+        if word == board[i][j]: # defualt case
+            return True;
+        if board[i][j] != word[0]:
+            return False;
+        # Creates a new inner list for every row in the outer list
+        temp = [row.copy() for row in board]
+        # or
+        temp = [list(row) for row in board]
+        temp[i][j] = "";
+        return self.search(temp, word[1:], i, j + 1) or self.search(temp, word[1:], i, j - 1) or self.search(temp, word[1:], i + 1, j) or self.search(temp, word[1:], i - 1, j);
+
+
